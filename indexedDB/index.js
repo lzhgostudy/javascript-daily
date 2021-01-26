40fe1458d701513de3d586e08792fd08d6037bb0
244_100007713101_luzhihua <623798099@qq.com>
2021-01-26 23:45:02 +0800

    IndexedDB learning

--- indexedDB/index.js
+++ indexedDB/index.js
@@ -7,50 +7,6 @@ function initDB() {
     alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.")
   }
 
-  const customerData = [
-    { 
-      ssn: "444-44-4444", 
-      name: "Bill", 
-      age: 35, 
-      email: "bill@company.com" 
-    },
-    { 
-      ssn: "555-55-5555", 
-      name: "Donna", 
-      age: 32, 
-      email: "donna@home.org" 
-    }
-  ];
-
-  var request = indexedDB.open("MyTestDatabase", 2);
-  var db;
-
-  request.onerror = function(event) {
-    console.log('db open fail');
-  }
-
-  request.onsuccess = function(event) {
-    console.log('数据库打开成功：', event);
-    db = event.target.result;
-
-    db.onerror = function(event) {
-      alert('Db error: ' + event.target.errorCode)
-    }
-
-    // var objectStore = db.createObjectStore('customers', { keyPath: 'ssn' });
-
-    // objectStore.createIndex("name", "name", { unique: false })
-
-    // objectStore.createIndex('email', 'email', { unique: true });
-
-    // objectStore.transaction.oncomplete = function(event) {
-    //   var customerObjectStore = db.transaction("customers", "readwrite").objectStore('customers');
-    //   customerData.forEach(function(customer) {
-    //     customerObjectStore.add(customer);
-    //   });
-    //   console.log('insert success!')
-    // }
-  }
 
   request.onupgradeneeded = function(event) {
     console.log('onupgradeneeded occur.')
@@ -75,6 +31,10 @@ function initDB() {
 function createObjectStore() {
   var request = indexedDB.open("MyTestDatabase", 3);
 
+  request.onsuccess = function(event) {
+    console.log('onsuccess occur.');
+  }
+
   request.onupgradeneeded = function(event) {
     console.log('onupgradeneeded occur.');
     var db = event.target.result;
@@ -97,12 +57,33 @@ function insert() {
     let customers13ObjStore = db.transaction('customers13', 'readwrite').objectStore('customers13');
 
     customers13ObjStore.add({
-      ssn: '1',
-      name: 'luzhihua',
-      email: '623798000@qq.com'
+      ssn: '2',
+      name: 'LU',
+      email: '2296889376@qq.com'
     });
 
     console.log('insert success');
+  }
+}
+
+function getDataList() {
+  var request = indexedDB.open('MyTestDatabase');
+
+  request.onsuccess = function(event) {
+    let db = event.target.result;
+
+    var objectStore = db.transaction('customers13').objectStore('customers13');
+
+    objectStore.openCursor().onsuccess = function(event) {
+      var cursor = event.target.result;
+      if (cursor) {
+        console.log("Name for SSN " + cursor.key + " is " + cursor.value.name);
+        cursor.continue();
+      }
+      else {
+        alert("No more entries!");
+      }
+    }
   }
 }
 
@@ -112,6 +93,11 @@ function getData() {
   request.onsuccess = function(event) {
     let db = event.target.result;
 
-    
+    let customerObjStore = db.transaction('customers13').objectStore('customers13');
+    let res = customerObjStore.get("1");
+    console.log('res: ', res)
+    res.onsuccess = function(event) {
+      console.log('getData: ', event.target.result)
+    }
   }
 }
